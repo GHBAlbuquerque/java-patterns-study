01a19165cb6efd72de45ebb7e9c3d4fa9d8366e9
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-12-06 15:55:27 -0300

    feature: logic improvements in AcquireLockUseCaseImpl

--- src/main/java/com/patterns/domain/usecase/AcquireLockUseCaseImpl.java
+++ src/main/java/com/patterns/domain/usecase/AcquireLockUseCaseImpl.java
@@ -8,11 +8,9 @@ import com.patterns.common.interfaces.usecases.AcquireLockUseCase;
 import com.patterns.external.database.orm.LockORM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-@Component
 public class AcquireLockUseCaseImpl implements AcquireLockUseCase {
 
     private final Logger log = LoggerFactory.getLogger(AcquireLockUseCaseImpl.class);
@@ -26,28 +24,22 @@ public class AcquireLockUseCaseImpl implements AcquireLockUseCase {
 
         LockORM existingLock = lockGateway.getLock(entityId);
 
-        if (existingLock != null) {
-            if (isLockActive(existingLock)) {
-                if (existingLock.getUserId().equals(userId)) {
-                    log.info("Lock for entityId {} already acquired by same user {}. Returning existing lock.", entityId, userId);
-                    return existingLock;
-                } else {
-                    log.warn("Lock for entityId {} already acquired by a different user {}. Current user {}.", entityId, existingLock.getUserId(), userId);
-                    throw new LockAlreadyAcquiredException(
-                        ExceptionCodesEnum.LOCK_01_ALREADY_ACQUIRED.name(),
-                        "Lock for entity " + entityId + " is already acquired by another user."
-                    );
-                }
-            } else {
-                log.info("Existing lock for entityId {} is expired. Creating a new lock.", entityId);
-            }
-        } else {
-            log.info("No existing lock found for entityId {}. Creating a new lock.", entityId);
+        if (existingLock == null || !isLockActive(existingLock)) {
+            final var expiresAt = LocalDateTime.now().plusSeconds(createLockDTO.duration());
+            final var newLockORM = new LockORM(userId, entityId, expiresAt);
+            return lockGateway.createLock(newLockORM);
         }
 
-        final var expiresAt = LocalDateTime.now().plusSeconds(createLockDTO.duration());
-        final var newLockORM = new LockORM(userId, entityId, expiresAt);
-        return lockGateway.createLock(newLockORM);
+        if (!existingLock.getUserId().equals(userId)) {
+            log.warn("Lock for entityId {} already acquired by a different user {}. Current user {}.", entityId, existingLock.getUserId(), userId);
+            throw new LockAlreadyAcquiredException(
+                ExceptionCodesEnum.LOCK_01_ALREADY_ACQUIRED.name(),
+                "Lock for entity " + entityId + " is already acquired by another user."
+            );
+        }
+
+        log.info("Lock for entityId {} already acquired by same user '{}'. Returning existing lock.", entityId, userId);
+        return existingLock;
     }
 
 }
